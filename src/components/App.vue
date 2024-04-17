c50496fcd2ee719d4cabbc6a7fb05b1669ac5f26
Matt Harrison <matt.harrison@traderinteractive.com>
2024-04-17 12:26:16 -0400

    initial commit.

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import type { Css } from 'tide-package-library-poc';
+  import { CSS, TideButton } from 'tide-package-library-poc';
+
+  const test: Css = CSS.BG_YELLOW;
+
+  console.log('const:', CSS);
+  // console.log('type:', Css);
+</script>
+
+<template>
+  <h1 :class="[CSS.MB_1]">TIDE Consumer App Demo</h1>
+  <TideButton :class="[CSS.MB_1]">Consumer</TideButton>
+  <TideButton :is-red="true">Consumer</TideButton>
+</template>
+
+<style scoped>
+</style>
